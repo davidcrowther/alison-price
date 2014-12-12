f76672cb3c862b6e41920c422d6d20d40fcbb996
dronni3de <example@email.com>
2014-12-12 15:20:23 +0200

    Init commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){  
+    
+    $(".page-header").anystretch('img/top-header.jpg');
+    $(".naturmort-1").anystretch('img/naturmort-1.jpg');
+    $(".expectations").anystretch('img/naturmort-2.jpg'); 
+
+  
+});
