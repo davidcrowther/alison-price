3c51254f58d349d5f1b7cadf6ad6e57cd72c6a9e
dronni3de <git@github.com>
2015-01-21 10:39:31 +0200

    test

--- js/custom.js
+++ js/custom.js
@@ -4,5 +4,49 @@ $(document).ready(function(){
     $(".naturmort-1").anystretch('img/naturmort-1.jpg');
     $(".expectations").anystretch('img/naturmort-2.jpg'); 
 
+   	$('form.signup-1').validate({
+	    rules: {
+  	      email: {
+	      	minlength: 2,
+	        required: true,
+	        email: true
+	      }  
+	    },
+	    highlight: function(label) {
+	    	$(label).closest('.form-control').addClass('has-error');
+	    },
+	    success: function(label) {
+	    	$(label).text('OK!').addClass('valid').closest('.form-control').addClass('has-success');
+
+	    }, 
+	    messages: { 
+	     email: {
+	       email: "Your email address must be in the format of 'email@yourdomain.com'"
+	      }
+	    }
+    });
+
+   	$('form.signup-2').validate({
+	    rules: {
+  	      email: {
+	      	minlength: 2,
+	        required: true,
+	        email: true
+	      }  
+	    },
+	    highlight: function(label) {
+	    	$(label).closest('.form-control').addClass('has-error');
+	    },
+	    success: function(label) {
+	    	$(label).text('OK!').addClass('valid').closest('.form-control').addClass('has-success');
+
+	    }, 
+	    messages: { 
+	     email: {
+	       email: "Your email address must be in the format of 'email@yourdomain.com'"
+	      }
+	    }
+    });
+
   
 });
